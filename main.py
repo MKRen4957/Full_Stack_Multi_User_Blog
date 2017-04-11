3eecdbed7f59d4aeae3dfe8b38dcce7dd9ef0aaa
Chudi Zhong <Chudi@Chudis-MBP-2.netgear.com>
2017-04-10 22:14:25 -0500

    Create signup, login and logout function. Create User and Post database. Create multiple templates.

--- main.py
+++ main.py
@@ -14,19 +14,101 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
+secret = "zx&ZAh%^E4IX@p"
+
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
+
+def make_secure_val(val):
+    return '%s|%s' % (val, hmac.new(secret, val).hexdigest())
+
+def check_secure_val(secure_val):
+    val = secure_val.split('|')[0]
+    if secure_val == make_secure_val(val):
+        return val
+
+def render_post(response, post):
+    response.out.write('<b>' + post.subject + '</b><br>')
+    response.out.write(post.content)
 
 class MainHandler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
+    def render_str(self, template, **params):
+        params['user'] = self.user
+        return render_str(template, **params)
+
     def render(self, template, **kw):
-        self.response.out.write(render_str(template, **kw))
+        self.write(render_str(template, **kw))
 
-# Basic Blog
+    def set_secure_cookie(self, name, val):
+        cookie_val = make_secure_val(val)
+        self.response.headers.add_header('Set-Cookie', '%s=%s; Path=/'
+            % (name, cookie_val))
 
+    def read_secure_cookie(self, name):
+        cookie_val = self.request.cookies.get(name)
+        return cookie_val and check_secure_val(cookie_val)
+
+    def login(self, user):
+        self.set_secure_cookie('user_id', str(user.key().id()))
+
+    def logout(self):
+        self.response.headers.add_header('Set-Cookie', 'user_id=; Path=/')
+
+    def initialize(self, *a, **kw):
+        webapp2.RequestHandler.initialize(self, *a, **kw)
+        uid = self.read_secure_cookie('user_id')
+        self.user = uid and User.by_id(int(uid))
+
+# Blog
+class Post(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    modified = db.DateTimeProperty(auto_now = True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
+
+class PostList(MainHandler):
+    def get(self):
+        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created")
+        self.render('list.html', posts = posts)
+
+class PostSingle(MainHandler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id))
+        post = db.get(key)
+        if not post:
+            self.error(404)
+            return
+        self.render("single.html", post = post)
+
+class NewPost(MainHandler):
+    def get(self):
+        if self.user:
+            self.render("newpost.html")
+        else:
+            self.redirect("/login")
+
+    def post(self):
+        if not self.user:
+            self.redirect('/list')
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+        if subject and content:
+            p = Post(subject = subject, content = content)
+            p.put()
+            self.redirect('/%s' % str(p.key().id()))
+        else:
+            error = "missing subject and content"
+            self.render("newpost.html", subject=subject, content=content, error=error)
+
+# Signup
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
 def valid_username(username):
     return username and USER_RE.match(username)
@@ -45,42 +127,105 @@ class Signup(MainHandler):
 
     def post(self):
         have_error = False
-        username = self.request.get('username')
-        password = self.request.get('password')
-        pw_retype = self.request.get('pw_retype')
-        email = self.request.get('email')
-
-        params = dict(username = username, email = email)
-
-        if not valid_username(username):
+        self.username = self.request.get('username')
+        self.password = self.request.get('password')
+        self.pw_retype = self.request.get('pw_retype')
+        self.email = self.request.get('email')
+        params = dict(username = self.username, email = self.email)
+        if not valid_username(self.username):
             params['error_username'] = "That's not a valid username."
             have_error = True
-
-        if not valid_password(password):
+        if not valid_password(self.password):
             params['error_password'] = "That wasn't a valid password."
             have_error = True
-        elif password != pw_retype:
+        elif self.password != self.pw_retype:
             params['error_retype'] = "Your passwords didn't match."
             have_error = True
-
-        if not valid_email(email):
+        if not valid_email(self.email):
             params['error_email'] = "That's not a valid email."
             have_error = True
-
         if have_error:
             self.render('signup.html', **params)
         else:
-            self.redirect('/welcome?username=' + username)
+            self.done()
 
-class Welcome(MainHandler):
-    def get(self):
-        username = self.request.get('username')
-        if valid_username(username):
-            self.render('welcome.html', username = username)
+    def done(self):
+        u = User.by_name(self.username)
+        if u:
+            msg = 'That user already exists.'
+            self.render('signup.html', error_username = msg)
         else:
-            self.redirect('/signup')
+            u = User.register(self.username, self.password, self.email)
+            u.put()
+            self.login(u)
+            self.redirect('/list')
+
+# User
+def make_salt(length = 5):
+    return ''.join(random.choice(letters) for x in xrange(length))
+
+def make_pw_hash(name, pw, salt = None):
+    if not salt:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s|%s' % (salt, h)
+
+def valid_pw(name, password, h):
+    salt = h.split('|')[0]
+    return h == make_pw_hash(name, password, salt)
+
+class User(db.Model):
+    name = db.StringProperty(required = True)
+    pw_hash = db.StringProperty(required = True)
+    email = db.StringProperty()
+
+    @classmethod
+    def by_id(cls, uid):
+        return User.get_by_id(uid)
+
+    @classmethod
+    def by_name(cls, name):
+        u = User.all().filter('name =', name).get()
+        return u
+
+    @classmethod
+    def register(cls, name, pw, email = None):
+        pw_hash = make_pw_hash(name, pw)
+        return User(name = name, pw_hash = pw_hash, email = email)
+
+    @classmethod
+    def login(cls, name, pw):
+        u = cls.by_name(name)
+        if u and valid_pw(name, pw, u.pw_hash):
+            return u
+
+# Login
+class Login(MainHandler):
+    def get(self):
+        self.render('login.html')
+
+    def post(self):
+        username = self.request.get('username')
+        password = self.request.get('password')
+        u = User.login(username, password)
+        if u:
+            self.login(u)
+            self.redirect('/list')
+        else:
+            msg = 'Invalid login'
+            self.render('login.html', error = msg)
+
+# Logout
+class Logout(MainHandler):
+    def get(self):
+        self.logout()
+        self.redirect('/list')
 
 app = webapp2.WSGIApplication([
     ('/signup', Signup),
-    ('/welcome', Welcome)
+    ('/list', PostList),
+    ('/([0-9]+)', PostSingle),
+    ('/newpost', NewPost),
+    ('/login', Login),
+    ('/logout', Logout),
     ], debug=True)
